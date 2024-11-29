4a08bcefc90c45338d5f7b55466fbd27fa0214a2
Rohan singh <151426524+Rohan6023@users.noreply.github.com>
2024-11-29 08:45:20 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7318195718:AAFnwUpr8GpNeirg9cEm000rvK8Vrkd_NZo")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("NINJAGAMEROP")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1240179115")
+
+
+
+
+
+
+
+  
